cee428b3f619960b61fb7587596981b673dd6e68
KunAn Hsu <annbigbig@gmail.com>
2020-08-27 17:17:16 +0800

    modified .eslintrc.js for suppressing Parse error : Unexpected token <

--- frontend-UI/apiez-ui/.eslintrc.js
+++ frontend-UI/apiez-ui/.eslintrc.js
@@ -1,13 +1,17 @@
 // .eslintrc.js
 // https://stackoverflow.com/questions/42706584/eslint-error-parsing-error-the-keyword-const-is-reserved
 // https://github.com/AtomLinter/linter-eslint/issues/462
+// https://github.com/vuejs/eslint-plugin-vue/issues/746
+// https://eslint.vuejs.org/user-guide/#faq
 module.exports = {
   "rules": {
     "no-mixed-spaces-and-tabs": 0, // disable rule
   },
-  "parser": "babel-eslint",
+  "extends": ["plugin:vue/base"],
+  "parser": "vue-eslint-parser",
   "parserOptions": {
-    "ecmaVersion": 2017,
+    "parser": "babel-eslint",
+    "ecmaVersion": 2020,
     "sourceType": "module"
   },
   "env": {
--- frontend-UI/apiez-ui/vue.config.js
+++ frontend-UI/apiez-ui/vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js
 module.exports = {
-   publicPath: '/'
-   //publicPath: '/apiez-ui/'
+   //publicPath: '/'
+   publicPath: '/apiez-ui/'
 }
